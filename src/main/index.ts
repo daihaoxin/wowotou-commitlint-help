b23fb309d40b4340074f0ff8522a8b445f4ee61e
daihaoxin <daihaoxin@126.com>
2020-10-12 18:05:24 +0800

    init

---
+++ src/main/index.ts
@@ -0,0 +1,5 @@
+import * as shell from 'shelljs';
+import './index2';
+// console.log(shell.exec('git status').code);
+// console.log(shell.exec('git rev-parse --show-toplevel'));
+console.log(33344);
---
+++ src/main/index2.ts
@@ -0,0 +1,4 @@
+import * as shell from 'shelljs';
+// console.log(shell.exec('git status').code);
+// console.log(shell.exec('git rev-parse --show-toplevel'));
+console.log(5554445);
