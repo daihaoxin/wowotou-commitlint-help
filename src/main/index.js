ad96f243e0f4cb8ea7af0898d58812ee004c19ec
daihaoxin <daihaoxin@126.com>
2020-10-19 18:42:59 +0800

    tsconfig.json

---
+++ src/main/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+require("./index2");
+// console.log(shell.exec('git status').code);
+// console.log(shell.exec('git rev-parse --show-toplevel'));
+console.log(33344);
---
+++ src/main/index2.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+// console.log(shell.exec('git status').code);
+// console.log(shell.exec('git rev-parse --show-toplevel'));
+console.log(5554445);
